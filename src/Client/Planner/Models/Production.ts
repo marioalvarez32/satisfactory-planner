4d718d7654770210283c77c0cef4a57303aa995d
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-11-19 14:47:21 -0600

    Adding the level to each recipe.

--- src/Client/Planner/Models/Production.ts
+++ src/Client/Planner/Models/Production.ts
@@ -4,11 +4,13 @@ export class Production {
   Input: ProductionInput[];
   Product: Item;
   ProductionRate: number;
+  Level: number;
 
   constructor(product: Production) {
     this.Product = product.Product;
     this.ProductionRate = product.ProductionRate;
     this.Input = product.Input.map((input) => new ProductionInput(input));
+    this.Level = product.Level;
   }
 }
 
