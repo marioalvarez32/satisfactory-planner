1d69b0647ce3c4b23176677485a575612e0d9951
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-11-20 22:23:49 -0600

    Grabbing the items from the json instead of hardconding them.

--- src/Client/Planner/Components/ProductionItem.vue
+++ src/Client/Planner/Components/ProductionItem.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-autocomplete v-model="value" variant="outlined" class="production-item" clearable item-title="Name" item-value="Name" :items="itemList" />
+  <v-autocomplete v-model="value" variant="outlined" class="production-item" item-title="Name" item-value="Name" :items="itemList" />
 </template>
 
 <script setup lang="ts">
   import { computed } from "vue"
+  import productionItems from "../Models/test.json"
+  import { uniqBy } from "lodash"
 
   const props = defineProps({
     modelValue: {
       type: String,
-      required: true,
+      default: "",
     },
   })
   const emit = defineEmits(["update:modelValue"])
@@ -18,10 +20,18 @@
       return props.modelValue
     },
     set(value) {
-      emit("update:modelValue", value)
+      if (value) {
+        emit("update:modelValue", value)
+      }
     },
   })
-  const itemList = [{ Name: "Iron Plate" }, { Name: "Iron Ingot" }, { Name: "Screw" }, { Name: "Iron Rod" }, { Name: "Iron Ore" }]
+  const itemList = uniqBy(productionItems.products, "Product").map(item => {
+    return {
+      Name: item.Product,
+    }
+  })
+
+  //const itemList = [{ Name: "Iron Plate" }, { Name: "Iron Ingot" }, { Name: "Screw" }, { Name: "Iron Rod" }, { Name: "Iron Ore" }]
 </script>
 
 <style lang="scss" scoped></style>
