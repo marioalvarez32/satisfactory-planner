c85b43fa20284f89c9602ebe251f915293653d78
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-11-24 15:08:23 -0600

    Adding support for json5

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPLugin from 'vite-plugin-eslint'
 import { resolve } from 'path'
+import json5Plugin from 'vite-plugin-json5'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
       vue: 'vue/dist/vue.esm-bundler.js',
     },
   },
-  plugins: [vue(), eslintPLugin()],
+  plugins: [vue(), eslintPLugin(), json5Plugin()],
 })
