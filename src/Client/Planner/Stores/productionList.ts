9c39f173424d29999358e094cbc12c832edbf426
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-11-25 18:59:15 -0600

    Linting with new printwidth.

--- src/Client/Planner/Stores/productionList.ts
+++ src/Client/Planner/Stores/productionList.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 import { ProductionListItem } from '../Models/ProductionList';
 import { Item } from '../Models/Item';
-import { getItemFromId, getOutputTotal } from '../Utilities/ItemUtility';
+import { getInputTotal, getItemFromId, getOutputTotal } from '../Utilities/ItemUtility';
 
 type State = {
   productionListItems: ProductionListItem[];
@@ -12,19 +12,15 @@ export const useProductionListStore = defineStore('production-list-store', {
   }),
   getters: {
     filteredProductionListItems(state): ProductionListItem[] {
-      return state.productionListItems.filter(
-        (productItem) => productItem.Id != ''
-      );
+      return state.productionListItems.filter((productItem) => productItem.Id != '');
     },
     items(): Item[] {
-      return this.filteredProductionListItems.map(
-        (listItem: ProductionListItem) => {
-          const item = getItemFromId(listItem.Id);
-          item.OutputRate = getOutputTotal(item);
-          item.InputRate = parseFloat(`${listItem.ItemsPerMinute}`);
-          return item;
-        }
-      );
+      return this.filteredProductionListItems.map((listItem: ProductionListItem) => {
+        const item = getItemFromId(listItem.Id);
+        item.OutputRate = getOutputTotal(item);
+        item.InputRate = getInputTotal(item);
+        return item;
+      });
     },
   },
   actions: {},
