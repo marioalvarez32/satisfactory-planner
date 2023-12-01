8e517d85f3ef11168a49206932e762d46d98a425
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-12-01 17:50:21 -0600

    Linting the router file.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,17 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import ProductionPlanner from '@/Client/Planner/ProductionPlanner.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import ProductionPlanner from '@/Client/Planner/ProductionPlanner.vue';
 
-const Home = { template: '<div>Home</div>' }
+const Home = { template: '<div>Home</div>' };
 
 const routes = [
   { path: '/', component: Home },
   { path: '/planner', component: ProductionPlanner },
-]
+];
 
 const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHashHistory(),
   routes, // short for `routes: routes`
-})
+});
 
-export { router }
+export { router };
