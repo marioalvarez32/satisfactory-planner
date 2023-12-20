7fad6fcab033930dc6ed88cd3281d6e104705611
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-12-20 16:10:02 -0600

    build: Adding logic to keep track of the application's version within the app using semantic release.

---
+++ updateInternalVersion.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import { readFile } from 'fs/promises';
+
+async function updateVersion(newVersion) {
+  console.log('🚀 ~ file: updateInternalVersion.js:5 ~ updateVersion ~ newVersion:', newVersion);
+  const customJsonFile = './internalVersion.json';
+
+  try {
+    const data = await readFile(customJsonFile, 'utf8');
+    const customJson = JSON.parse(data);
+
+    customJson.version = newVersion;
+
+    fs.writeFileSync(customJsonFile, JSON.stringify(customJson, null, 2));
+    console.log(`Updated version in ${customJsonFile} to ${newVersion}`);
+  } catch (error) {
+    console.error(`Error updating version: ${error}`);
+  }
+}
+
+// eslint-disable-next-line no-undef
+const newVersion = process.argv[2];
+updateVersion(newVersion);
