39bd7c231c3f3fed8408818f02f3d24d1c11b341
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2023-12-20 17:55:44 -0600

    fix: Trying another way to fix semantic release.

--- src/Client/NavigationBar.vue
+++ src/Client/NavigationBar.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-  import { version } from "@/../public/internalVersion.json"
+  import { version } from "@/../internalVersion.json"
 </script>
 
 <style lang="scss" scoped>
--- updateInternalVersion.mjs
+++ updateInternalVersion.mjs
@@ -3,7 +3,7 @@ import { readFile } from 'fs/promises';
 
 async function updateVersion(newVersion) {
   console.log('🚀 ~ file: updateInternalVersion.js:5 ~ updateVersion ~ newVersion:', newVersion);
-  const customJsonFile = './public/internalVersion.json';
+  const customJsonFile = './internalVersion.json';
 
   try {
     const data = await readFile(customJsonFile, 'utf8');
